aef90a64974ed1c5bf63600aa6a004244b15964a
hubagruba <nastena.gruba@gmail.com>
2020-10-01 11:56:36 +0300

    Cross-platform technologies

---
+++ lab2/NetBeans_projects/MyApp/src/acrostic/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package acrostic;
+
+import org.me.mylib.LibClass;
+
+/**
+ *
+ * @author naste
+ */
+public class Main {
+
+       
+    public static void main(String[] args) {
+         String result = LibClass.acrostic(args);
+         System.out.println("Result="+result);
+    }
+
+}
